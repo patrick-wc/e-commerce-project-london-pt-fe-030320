0ee568286ccf327446aacb1138152eafd105e690
Patrick Whitty-Clarke <patrick@prefacestudios.com>
2020-04-26 12:53:49 +0100

    most of JS done. created object with user selected tax terms arrays. need to render products with them next

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -3,34 +3,7 @@
 // *********************
 const products_ul = document.querySelector(".products");
 
-// clear products
-// products_ul.innerHTML = "";
-
-products.forEach((product) => {
-  // console.log(product);
-
-  // <li class="product">
-  //     <img
-  //     class="product__image"
-  //     src="images/products/sofa-1.jpg"
-  //     alt=""
-  //     />
-  //     <a class="product__link" href=""
-  //     ><h2 class="product__title">Yenings</h2></a
-  //     >
-  //     <div class="product__details">
-  //     <p class="product__cat">Sofa</p>
-  //     <p class="product__price">£2,600</p>
-  //     </div>
-  //     <button class="product__add">
-  //     <img
-  //         src="images/icons/cart.svg"
-  //         width="24"
-  //         height="24"
-  //         alt="add to cart"
-  //     />
-  //     </button>
-  // </li>
+const renderProduct = (product) => {
   const li = document.createElement("li");
   li.classList.add("product");
 
@@ -74,31 +47,190 @@ products.forEach((product) => {
   li.append(add);
 
   products_ul.append(li);
+};
+
+const renderProducts = (productsToRender) => {
+  products_ul.innerHTML = "";
+
+  productsToRender.forEach((product) => {
+    renderProduct(product);
+  });
+};
+
+renderProducts(products);
+
+// *********************
+// range / category filtering stuff
+// *********************
+
+const uniqueColorsArrays = products.map((p) => p.colors);
+
+// I've returned 20 arrays of colors
+// I want to extract each individual color combination from those arrays, and then build filters from those options
+// so...
+
+// maybe I should be using one of the more fancy functions from ex37 here? I dunno, please let me know if so:
+let uniqueColorsValuesArray = [];
+uniqueColorsArrays.forEach((color, i) => {
+  // for of loop because array
+  for (let index = 0; index < color.length; index++) {
+    const element = color[index];
+    uniqueColorsValuesArray.push(element);
+  }
 });
+
+// now I have the array, I need remove all duplicates:
+// console.log(uniqueColorsValuesArray);
+
+// you can do this via Set, filter or reduce apparently https://medium.com/dailyjs/how-to-remove-array-duplicates-in-es6-5daa8789641c
+// I'm going to do it via all 3 methods to help it stick in my brain
+// Set:
+
+// create a new ES6 object data type of Set:
+// const uniqueSet = new Set(uniqueColorsValuesArray);
+
+// // convert the object back into an array:
+// const uniqueColorsValuesProper = [...uniqueSet];
+
+// // could also use Array.from and keep the same variable name
+uniqueColorsValuesArray = Array.from(new Set(uniqueColorsValuesArray));
+// console.log(uniqueColorsValuesArray);
+
+// console.log(uniqueColorsValuesProper);
+
+// filter
+// uniqueColorsValuesArrayFilter = uniqueColorsValuesArray.filter(
+//   (item, index) => uniqueColorsValuesArray.indexOf(item) === index
+// );
+
+// // so the filter method returns a new array
+// // item = the color
+// // index = the loop # of the filter method
+// // uniqueColorsValuesArray.indexOf(item) = the first index found for that item
+// // conditional = uniqueColorsValuesArray.indexOf(item) === true
+// // if the first index found matches the item being processed, return true
+// // otherwise, its not unique so exclude it from the array
+// // so the new array is populated with only unique items
+// console.log(uniqueColorsValuesArrayFilter);
+
+// reduce:
+// this is the tough one from ex37 that I struggled with.
+// apparently even Google engineers argued whether it should ever be used
+// I'm gonna leave this until I finish the project
+// uniqueColorsValuesArrayReduce = uniqueColorsValuesArray.reduce(
+//   (unique, (item) => (unique.includes(item) ? unique : [...unique, item]), [])
+// );
+// console.log(uniqueColorsValuesArrayReduce);
+
+// categories
+let uniqueCategories = products.map((p) => p.type);
+
+uniqueCategories = Array.from(new Set(uniqueCategories));
+const colorsCheckboxContainer = document.querySelector(
+  "ul[data-filter-tax=colors]"
+);
+const typesCheckboxContainer = document.querySelector(
+  "ul[data-filter-tax=types]"
+);
+// tried to do
+const userCheckedTaxes = {};
+// but couldn't work out how to do it without a data-type on the input, and some other steps
+
+const userCheckedColors = [];
+const userCheckedTypes = [];
+
+const applyTaxEventListener = (input, tax) => {
+  input.addEventListener("change", (event) => {
+    console.log(`input changed ${event.target.value}`);
+    const value = event.target.value;
+
+    if (userCheckedTaxes.hasOwnProperty(tax)) {
+      // console.log("tax object property already exists");
+
+      if (userCheckedTaxes[tax].includes(value)) {
+        // console.log("we already have this value, so remove it");
+
+        // I think there might be a smarter ES6 function here, maybe .filter?
+        // find index:
+        let index = userCheckedTaxes[tax].findIndex(
+          (taxTerm) => taxTerm === value
+        );
+
+        userCheckedTaxes[tax].splice(index, 1);
+      } else {
+        // console.log("we do not have this value yet, add it");
+        userCheckedTaxes[tax].push(value);
+      }
+    } else {
+      // console.log("tax object property does not exist");
+
+      let checkedTax = [];
+      checkedTax.push(value);
+      userCheckedTaxes[tax] = checkedTax;
+    }
+
+    // now we have an object with the types arrays, we want to render products with those selected taxTerms:
+    // so, first get array of products by those terms...
+
+    // then render Products:
+    // renderProducts();
+  });
+};
+
+const renderTaxes = (taxes, tax) => {
+  const container = document.querySelector(`ul[data-filter-tax=${tax}]`);
+  // clear colors first
+  container.innerHTML = "";
+
+  // sort colors array by alphabetical
+  taxes.sort();
+
+  taxes.forEach((taxTerm) => {
+    const li = document.createElement("li");
+
+    const label = document.createElement("label");
+    label.innerText = taxTerm;
+    label.setAttribute("for", taxTerm);
+
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.value = taxTerm;
+    input.name = taxTerm;
+    input.id = taxTerm;
+
+    li.append(input);
+    li.append(label);
+    container.append(li);
+
+    // add event listeners to checkbox:
+    applyTaxEventListener(input, tax);
+  });
+};
+
+renderTaxes(uniqueColorsValuesArray, "colors");
+renderTaxes(uniqueCategories, "types");
 
 const range = document.querySelector(".range__input");
+const rangeScaleLow = document.querySelector(".range__scale--low");
+const rangeScaleHigh = document.querySelector(".range__scale--high");
 
-let productPrices = [];
-products.forEach((product) => {
-  console.log(product.price);
+const lowestProductPrice = [...products].sort((a, b) => a.price - b.price)[0]
+  .price;
+const highestProductPrice = [...products].sort((a, b) => b.price - a.price)[0]
+  .price;
 
-  productPrices.push(product.price);
-});
+rangeScaleLow.innerText = `$${lowestProductPrice}`;
+rangeScaleHigh.innerText = `$${highestProductPrice}`;
 
-console.log(productPrices);
-
-const rangeFrom = Math.min(...productPrices);
-console.log(rangeFrom);
-
-const rangeTo = Math.max(...products.price);
-console.log(rangeTo);
+range.setAttribute("min", lowestProductPrice);
+range.setAttribute("max", highestProductPrice);
 
 range.addEventListener("change", () => {
   console.log(range.value);
   const from = parseInt(0);
   const to = parseInt(range.value);
 
-  filteredProducts = products.filter((product) => {
+  const filteredProducts = products.filter((product) => {
     // return true or false;
     if (product.price >= from && product.price <= to) {
       return true;
@@ -107,5 +239,5 @@ range.addEventListener("change", () => {
     }
   });
 
-  console.log(filteredProducts);
+  renderProducts(filteredProducts);
 });
